9abf8bddced4a197a4b254ae531306985cc36743
MateoZouko <mateozuko1@gmail.com>
2023-09-04 13:09:25 +0000

    task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,35 +1,14 @@
 #include "binary_trees.h"
 
-binary_tree_t
-*binary_tree_insert_right(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new_node;
-
-	if (parent == NULL)
+void
+binary_tree_delete(binary_tree_t *tree) {
+	if (tree == NULL)
 	{
-		return (NULL);
+		return;
 	}
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-
-	if (parent->right != NULL)
-	{
-		new_node->right = parent->right;
-		parent->right->parent = new_node;
-	}
-	else
-	{
-		new_node->right = NULL;
-	}
-
-	parent->right = new_node;
-	return (new_node);
+	free(tree);
 }
