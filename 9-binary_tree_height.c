8cbb8c1f2053face4e61f2532bc958184d813bc7
MateoZouko <mateozuko1@gmail.com>
2023-09-04 13:51:09 +0000

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,14 +3,17 @@
 size_t
 binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_count = 0, right_count = 0;
 
 	if (tree == NULL)
 		return (0);
+	if (tree->left != NULL)
+		leftCount = 1 + binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		rightCount = 1 + binary_tree_height(tree->right);
+	if (leftCount > rightCount)
+		return (leftCount);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height) ? (left_height + 1) : (right_height + 1));
+	return (right_count);
 }
 
